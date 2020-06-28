4b6b7cdc4e5e0143910c39069387ae894f2929ef
wonsim00 <mspython@naver.com>
2020-06-28 23:27:26 +0900

    add properties & types

--- resource/channel.py
+++ resource/channel.py
@@ -1,5 +1,6 @@
 from .resource import Resource
 from .message import Message
+from utils.decorators import cached
 
 class Channel(Resource):
     def __init__(self, **kwargs):
@@ -11,6 +12,16 @@ class Channel(Resource):
     def messages(self):
         for message in self.__messages:
             yield message
+    
+    @property
+    def oldest_message_id(self):
+        if len(self.__messages):
+            return self.__messages[-1].id
+        return None
+    
+    @property
+    def url_messages(self):
+        return f'/channels/{self.id}/messages'
     
     def add_message(self, message):        
         if not isinstance(message, Message):
@@ -26,3 +37,39 @@ class Channel(Resource):
     @staticmethod
     def from_dict(kwargs):
         return Channel(**kwargs)
+    
+    def __repr__(self):
+        return f'<Channel {self.id} of name `{self.name}` and type `{types.from_index(self.type)}`>'
+
+class _Types:
+    __keys = cached([
+        'GUILD_TEXT',
+        'DM',
+        'GUILD_VOICE',
+        'GROUP_DM',
+        'GUILD_CATEGORY',
+        'GUILD_NEWS',
+        'GUILD_STORE'
+    ])
+    __obj = None
+
+    @staticmethod
+    def get_object():
+        if _Types.__obj == None:
+            _Types.__obj = _Types()
+        return _Types.__obj
+    
+    def __init__(self):
+        self.__indices = {}
+        for idx, key in enumerate(self.__keys):
+            self.__indices[key] = idx
+    
+    def __getattr__(self, key):
+        if key in self.__indices:
+            return self.__indices[key]
+        raise IndexError
+    
+    def from_index(self, index):
+        return self.__keys[index]
+
+types = _Types.get_object()
