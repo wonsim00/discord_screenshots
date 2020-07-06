351db49123212cdeafa935acfffedc2bff9fd3ae
wonsim00 <mspython@naver.com>
2020-07-06 16:40:19 +0900

    bugfix

--- utils/image.py
+++ utils/image.py
@@ -1,5 +1,5 @@
-import PIL as _PIL
+import PIL.Image as _Image
 import io as _io
 
 def load_image_from_bytes(bytes):
-    return _PIL.Image.open(_io.BytesIO(bytes))
+    return _Image.open(_io.BytesIO(bytes))
