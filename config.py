f5a087a320ffb567a0b3d46397c84f34f610b50b
wonsim00 <mspython@naver.com>
2020-07-13 22:25:37 +0900

    add config.py

---
+++ config.py
@@ -0,0 +1,36 @@
+import json as _json
+
+class _Config:
+    def __init__(self, config_json):
+        if isinstance(config_json, dict):
+            super(_Config, self).__setattr__("_Config__value", {})
+            for key in config_json:
+                self.__value[key] = _Config(config_json[key])
+        else:
+            super(_Config, self).__setattr__("_Config__value", config_json)
+    
+    def __getattr__(self, key):
+        if isinstance(self.__value, dict) and key in self.__value:
+            return self.__value[key].__get__(None, None)
+        return super(_Config, self).__getattribute__(key)
+    
+    def __setattr__(self, key, value):
+        if isinstance(self.__value, dict):
+            if key not in self.__config:
+                self.__config[key] = value
+            else:
+                raise AttributeError
+        else:
+            return super(_Config, self).__setattr__(key, value)
+    
+    def __get__(self, instance, owner = None):
+        if isinstance(self.__value, dict):
+            return self
+        else:
+            return self.__value
+    
+    def __set__(self, instance, value):
+        raise AttributeError
+
+with open("config.json", "r", encoding = "utf8") as f:
+    config = _Config(_json.loads(f.read()))
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from threading import Thread
 from app import app
+from config import config
 import time
 import webview
 
+port = config.app_configurations.port
+open_window_after = config.app_configurations.open_window_after
+title = config.app_configurations.title
+width = config.app_configurations.width
+height = config.app_configurations.height
+
 def run_app():
-    app.run()
+    app.run(port = port)
 
 t = Thread(target=run_app)
 t.daemon = True
 t.start()
 
-time.sleep(1)
+time.sleep(open_window_after)
 window = webview.create_window(
-    title = 'Discord Screenshots',
-    url = 'http://127.0.0.1:5000/',
-    width = 600,
-    height = 800
+    title = title,
+    url = f'http://127.0.0.1:{port}/',
+    width = width,
+    height = height
 )
 webview.start()
